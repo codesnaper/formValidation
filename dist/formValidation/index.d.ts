29ba35d3abb5769caa15ba9cf058db27311677f7
SINA <sinarani6633@gmail.com>
2023-01-06 14:50:58 +0100

    (chore) Remove dist file

--- dist/formValidation/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { FormValidation, Validations } from "../model";
-export declare const useFormValidation: <T extends Record<keyof T, any> = {}>(options?: {
-    validations?: Partial<Record<keyof T, import("../model").Validation<T>>>;
-    initialValues?: Partial<T>;
-    onSubmit?: () => void;
-}) => FormValidation<T>;
--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.useFormValidation = void 0;
-var formValidation_1 = require("./formValidation");
-Object.defineProperty(exports, "useFormValidation", { enumerable: true, get: function () { return formValidation_1.useFormValidation; } });
